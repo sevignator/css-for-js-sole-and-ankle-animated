e0230eedd09cab6b3ce0c19f05e39dbd7448c62c
Mikaël Sévigny <sevignator@hotmail.com>
2023-10-10 19:33:02 -0400

    Add solution to Exercise 3

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import styled from 'styled-components/macro';
+import styled, { keyframes } from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
 import { QUERIES, WEIGHTS } from '../../constants';
@@ -12,39 +12,68 @@ import VisuallyHidden from '../VisuallyHidden';
 const MobileMenu = ({ isOpen, onDismiss }) => {
   return (
     <Overlay isOpen={isOpen} onDismiss={onDismiss}>
-      <Content aria-label="Menu">
+      <Backdrop />
+      <Content aria-label='Menu'>
         <CloseButton onClick={onDismiss}>
-          <Icon id="close" />
+          <Icon id='close' />
           <VisuallyHidden>Dismiss menu</VisuallyHidden>
         </CloseButton>
-        <Filler />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Footer>
-          <SubLink href="/terms">Terms and Conditions</SubLink>
-          <SubLink href="/privacy">Privacy Policy</SubLink>
-          <SubLink href="/contact">Contact Us</SubLink>
-        </Footer>
+        <SubWrapper>
+          <Filler />
+          <Nav>
+            <NavLink href='/sale'>Sale</NavLink>
+            <NavLink href='/new'>New&nbsp;Releases</NavLink>
+            <NavLink href='/men'>Men</NavLink>
+            <NavLink href='/women'>Women</NavLink>
+            <NavLink href='/kids'>Kids</NavLink>
+            <NavLink href='/collections'>Collections</NavLink>
+          </Nav>
+          <Footer>
+            <SubLink href='/terms'>Terms and Conditions</SubLink>
+            <SubLink href='/privacy'>Privacy Policy</SubLink>
+            <SubLink href='/contact'>Contact Us</SubLink>
+          </Footer>
+        </SubWrapper>
       </Content>
     </Overlay>
   );
 };
 
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const slideIn = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+
+  to {
+    transform: translateX(0);
+  }
+`;
+
 const Overlay = styled(DialogOverlay)`
   position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: var(--color-backdrop);
+  inset: 0;
   display: flex;
   justify-content: flex-end;
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
+  inset: 0;
+  background-color: var(--color-backdrop);
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${fadeIn} 500ms both ease-out;
+  }
 `;
 
 const Content = styled(DialogContent)`
@@ -52,8 +81,11 @@ const Content = styled(DialogContent)`
   width: 300px;
   height: 100%;
   padding: 24px 32px;
-  display: flex;
-  flex-direction: column;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${slideIn} 300ms both cubic-bezier(0.17, 0.24, 0.25, 1);
+    animation-delay: 150ms;
+  }
 `;
 
 const CloseButton = styled(UnstyledButton)`
@@ -61,6 +93,17 @@ const CloseButton = styled(UnstyledButton)`
   top: 10px;
   right: 0;
   padding: 16px;
+`;
+
+const SubWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${fadeIn} 500ms both;
+    animation-delay: 300ms;
+  }
 `;
 
 const Nav = styled.nav`
